e8972f1b05e41ef8a735ae495f4a53c013b73621
Ayobami Ekundayo <ayobamieae@gmail.com>
2025-09-02 11:53:38 +0100

    Updated initial commit

--- src/components/anniversary/AnniversaryHero.tsx
+++ src/components/anniversary/AnniversaryHero.tsx
@@ -18,7 +18,7 @@ const heroImages: HeroImage[] = [
     backgroundSize: "cover",
     backgroundPosition: "center 30%",
     mobileBackgroundSize: "cover",
-    mobileBackgroundPosition: "center left"
+    mobileBackgroundPosition: "center left 50%"
   },
   {
     src: "/uploads/3f4cc98c-f210-432a-b840-defc0e94841f.png",
